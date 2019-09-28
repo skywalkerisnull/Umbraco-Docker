098e771d09360edd12aa042453d2a3c8ea790609
Ashley Walker <ashley.walker@infarm.io>
2019-09-28 14:52:22 +1000

    Initial build w/ finding latest version of Umbraco

---
+++ build.ps1
@@ -0,0 +1,26 @@
+# Find the latest version of Umbraco
+"Please note, this method may be flakey as it is using the NuGet directory that may be out of sync with the download location of the pre-built version of Umbraco that can be otherwise downloaded"
+Function Get-RedirectedUrl {
+	Param (
+		[Parameter(Mandatory=$true)]
+		[String]$URL
+	)
+	$request = [System.Net.WebRequest]::Create($url)
+	$request.AllowAutoRedirect=$false
+	$response=$request.GetResponse()
+	If ($response.StatusCode -eq "Found")
+	{
+		$response.GetResponseHeader("Location")
+	}
+}
+
+$FileName = [System.IO.Path]::GetFileName((Get-RedirectedUrl "https://www.nuget.org/api/v2/package/UmbracoCms"))
+$FileName = [System.IO.Path]::GetFileNameWithoutExtension($FileName)
+$Version = $FileName.Replace("umbracocms.","")
+
+""
+"Docker will build Umbraco on Version: $Version" 
+""
+
+# Build the docker image using the above version
+docker build --build-arg version=$Version .
